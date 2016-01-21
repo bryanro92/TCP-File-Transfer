30ed459949a8e1addbecbdc6aaf618247a96e984
Ross Bryan <bryanro@gvsu.e.edu>
2016-01-21 14:54:13 -0500

    crikey m8

--- client.java
+++ client.java
@@ -9,30 +9,80 @@ import java.net.*;
 import java.util.Scanner;
 
 public class client {
-public static void main(String[] args) throws Exception{
-	
-	Scanner sc = new Scanner(System.in);
-	System.out.print("Ay m8: Enter in a port: ");
-	String uSocket = sc.next();
-	System.out.println();
-	System.out.print("Crikey m8, we need an IP Address to connect to: ");
-	String uIP = sc.next();
 
- 	Socket clientSocket = new Socket("127.0.0.1", 9876);
-	// Socket clientSocket = new Socket(uIP, Integer.parseInt(uSocket));
+	private void clientConnect(){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	System.out.print("Ay m8: Enter in a port: ");
+
+
+	//Requests PORT and IP from CLIENT
+	try {
+		uSocket = br.readLine();
+	}
+	catch (IOException ioe){
+		System.out.println("Bloody hell m8 dat port ain't rite");
+		System.exit(1);
+	}
+	System.out.print("Crikey m8, we need an IP Address to connect to: ");
+	try {
+		uIP = br.readLine();
+	} catch (IOException ioe) {
+		System.out.println("no no no. IP Address failed");
+		System.exit(1);
+	}
+	System.out.println("We'll get right on connecting you to: " + uIP +
+		"\nOn socket: " + uSocket);
+
+
+	try {
+		Socket clientSocket = new Socket("127.0.0.1", 9876);
+		// Socket clientSocket = new Socket(uIP, Integer.parseInt(uSocket));
+	}
+	catch (Exception e){
+		System.out.println("Hm m8 dat didn't work our quite well. no connection");
+		System.exit(1);
+	}
+
+
+	}
+
+
+public static void main(String[] args){
+	String uSocket = "";
+	String uIP = "";
+	String file = "";
+	System.out.println("M8! Surfs up");
+
+	//grabs port and ip from client. error checks. if pass this then we
+	//have connected to server
+	clientConnect();
+	
+	System.out.println("Crikey! Succesfully connected to server");
+
+	//lets set up our buffered readers now shall we
+	DataOutputStream outToServer =
+		new DataOutputStream(clientSocket.getOutputStream());
+	BufferedReader inFromServer =
+		new BufferedReader(
+			new InputStreamReader(clientSocket.getInputStream()));
+	BufferedReader inFromUser =
+		new BufferedReader(new InputStreamReader(System.in));
+
+
 
  	System.out.println("Enter in a file to download m8: ");
- 	String file = sc.next();
+
+	String fileName = inFromUser.readLine();
+	//sends over socket
+	outToServer.writeBytes(fileName+"\n");
+
+	System.out.println("Client is requesting file: " + fileName);
+
+
  	System.out.println("You want to download " + file
  		+ " m8? Crikey");
  	
-
-
-
-
-
-
-
 
 
 
--- server.java
+++ server.java
@@ -6,22 +6,31 @@ CIS 457
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class server{
 public static void main(String[] args) throws Exception {
+	//this is for part 2 - maintains the number of connections
+	ArrayList<Socket> connections = new ArrayList<Socket>();
+
+	String sSocket = "";
+
 	System.out.println("Knockin up the FTP server like a mad woman's breakfast");
 	System.out.print("Oi, we need a socket to listen to m8: ");
+
 	Scanner sc = new Scanner(System.in);
 	String uSocket = sc.next();
 	System.out.println();
 	ServerSocket listenSocket = new ServerSocket(9876);
 	//ServerSocket listenSocket = new ServerSocket(Integer.parseInt(uSocket));
-	System.out.println("Server listening from the land down under " +
+	System.out.println("Server listening...\nfrom the land down under \n" +
 			"on port: \n" + listenSocket + "\n");
 	while (true){
 		Socket connectionSocket = listenSocket.accept();
+		connections.add(connectionSocket);
 		System.out.println("Ready the barbie a m8 has connected.");
+		System.out.println("There is " + connections.size() + " m8(s) connected.");
 		//returns a new socket object that represents server to a specific client
 		BufferedReader inFromClient = new BufferedReader(
 			new InputStreamReader(connectionSocket.getInputStream()));
@@ -29,6 +38,7 @@ public static void main(String[] args) throws Exception {
 			connectionSocket.getOutputStream());
 		String clientMessage = inFromClient.readLine();
 		System.out.println("The client said: " + clientMessage);
+
 
 
 
