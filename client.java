45415d83eeaf51805d23551aa91aec84fec18e85
Ross Bryan <bryanro@gvsu.e.edu>
2016-01-20 22:46:18 -0500

    some updates

--- client.java
+++ client.java
@@ -1,15 +1,16 @@
 /*
-Ross Bryan and Nate Lentz
-CIS 457
+Ross Bryan
 Project 1
-Client side 
+CIS 457
+1/20/16
 */
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-class client{
-public static void main(String args[]) throws Exception{
+public class client {
+public static void main(String[] args) throws Exception{
+	
 	Scanner sc = new Scanner(System.in);
 	System.out.print("Ay m8: Enter in a port: ");
 	String uSocket = sc.next();
@@ -17,31 +18,27 @@ public static void main(String args[]) throws Exception{
 	System.out.print("Crikey m8, we need an IP Address to connect to: ");
 	String uIP = sc.next();
 
-	Socket clientSocket = new Socket("127.0.0.1", 9876);
+ 	Socket clientSocket = new Socket("127.0.0.1", 9876);
 	// Socket clientSocket = new Socket(uIP, Integer.parseInt(uSocket));
-	DataOutputStream outToServer =
-		new DataOutputStream(clientSocket.getOutputStream());
-	BufferedReader inFromServer =
-		new BufferedReader(
-			new InputStreamReader(clientSocket.getInputStream()));
-	BufferedReader inFromUser =
-		new BufferedReader(new InputStreamReader(System.in));
 
-		//gets data form user
-	System.out.println("Enter a message: ");
-	String message = inFromUser.readLine();
-	//sends over socket
-	outToServer.writeBytes(message+"\n");
+ 	System.out.println("Enter in a file to download m8: ");
+ 	String file = sc.next();
+ 	System.out.println("You want to download " + file
+ 		+ " m8? Crikey");
+ 	
 
-	System.out.println("Ok, I sent it");
 
-	String serverMessage = inFromServer.readLine();
-	System.out.println("Message: " + serverMessage);
-	clientSocket.close();
+
+
+
+
+
+
+
+
+
+
 }
-
-
-
 
 
 }
--- server.java
+++ server.java
@@ -8,28 +8,33 @@ import java.net.*;
 import java.util.Scanner;
 
 
-public class tcpserver{
-	public static void main(String[] args) throws Exception {
-		System.out.print("Oi, we need a socket to listen to m8: ");
-		Scanner sc = new Scanner(System.in);
-		String uSocket = sc.next();
-		ServerSocket listenSocket = new ServerSocket(9876);
-		//ServerSocket listenSocket = new ServerSocket(Integer.parseInt(uSocket));
-		while (true){
-			Socket connectionSocket = listenSocket.accept();
-			//returns a new socket object that represents server to a specific client
-			BufferedReader inFromClient = new BufferedReader(
-				new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(
-				connectionSocket.getOutputStream());
-			String clientMessage = inFromClient.readLine();
-			System.out.println("The client said: " + clientMessage);
+public class server{
+public static void main(String[] args) throws Exception {
+	System.out.println("Knockin up the FTP server like a mad woman's breakfast");
+	System.out.print("Oi, we need a socket to listen to m8: ");
+	Scanner sc = new Scanner(System.in);
+	String uSocket = sc.next();
+	System.out.println();
+	ServerSocket listenSocket = new ServerSocket(9876);
+	//ServerSocket listenSocket = new ServerSocket(Integer.parseInt(uSocket));
+	System.out.println("Server listening from the land down under " +
+			"on port: \n" + listenSocket + "\n");
+	while (true){
+		Socket connectionSocket = listenSocket.accept();
+		System.out.println("Ready the barbie a m8 has connected.");
+		//returns a new socket object that represents server to a specific client
+		BufferedReader inFromClient = new BufferedReader(
+			new InputStreamReader(connectionSocket.getInputStream()));
+		DataOutputStream outToClient = new DataOutputStream(
+			connectionSocket.getOutputStream());
+		String clientMessage = inFromClient.readLine();
+		System.out.println("The client said: " + clientMessage);
 
 
 
-			outToClient.writeBytes(clientMessage+"\n");
+		outToClient.writeBytes(clientMessage+"\n");
 
-			connectionSocket.close();
+		connectionSocket.close();
 
 		}
 
