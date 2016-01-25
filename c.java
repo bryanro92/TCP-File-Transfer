2888a4a9c93a2a2583565f05a091b2633811e350
Ross Bryan <bryanro@gvsu.e.edu>
2016-01-25 11:11:49 -0500

    commit often

---
+++ c.java
@@ -0,0 +1,109 @@
+/*
+Ross Bryan
+Project 1
+CIS 457
+1/20/16
+*/
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class c {
+
+	private void clientConnect(){
+		
+
+	}
+
+
+public static void main(String[] args) throws Exception{
+	String uSocket = "";
+	String uIP = "";
+	String file = "";
+	System.out.println("M8! Surfs up");
+	Socket socket = null;
+
+	//grabs port and ip from client. error checks. if pass this then we
+	//have connected to server
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	System.out.print("Ay m8: Enter in a port: ");
+
+
+	//Requests PORT and IP from CLIENT
+	try {
+		uSocket = br.readLine();
+	}
+	catch (IOException ioe){
+		System.out.println("Bloody hell m8 dat port ain't rite");
+		System.exit(1);
+	}
+	System.out.print("Crikey m8, we need an IP Address to connect to: ");
+	try {
+		uIP = br.readLine();
+	} catch (IOException ioe) {
+		System.out.println("no no no. IP Address failed");
+		System.exit(1);
+	}
+	System.out.println("We'll get right on connecting you to: " + uIP +
+		"\nOn socket: " + uSocket);
+
+
+	try {
+		socket = new Socket("127.0.0.1", 9876);
+		// Socket clientSocket = new Socket(uIP, Integer.parseInt(uSocket));
+	}
+	catch (Exception e){
+		System.out.println("Hm m8 dat didn't work our quite well. no connection");
+		System.exit(1);
+	}
+
+	
+	System.out.println("Crikey! Succesfully connected to server");
+
+	//lets set up our buffered readers now shall we
+	DataOutputStream outToServer =
+		new DataOutputStream(socket.getOutputStream());
+	BufferedReader inFromServer =
+		new BufferedReader(
+			new InputStreamReader(socket.getInputStream()));
+	BufferedReader inFromUser =
+		new BufferedReader(new InputStreamReader(System.in));
+
+
+
+ 	System.out.print("Enter in a file to download m8: ");
+ 	System.out.println();
+	String fileName = inFromUser.readLine();
+	//sends over socket
+	//hard coded for testing
+	outToServer.writeBytes(fileName +"\n");
+
+
+	System.out.println("Server will fetch: " + fileName);
+
+
+int filesize=1022386; 
+int bytesRead; 
+int currentTot = 0;
+byte [] bytearray = new byte [filesize];
+  InputStream is = socket.getInputStream(); 
+  FileOutputStream fos = new FileOutputStream("copy.txt"); 
+  BufferedOutputStream bos = new BufferedOutputStream(fos); 
+  bytesRead = is.read(bytearray,0,bytearray.length); 
+  currentTot = bytesRead; 
+  do { bytesRead = is.read(bytearray, currentTot, (bytearray.length-currentTot)); 
+    if(bytesRead >= 0) currentTot += bytesRead; 
+  } while(bytesRead > -1); 
+   bos.write(bytearray, 0 , currentTot);
+   bos.flush(); 
+   bos.close(); 
+   socket.close();
+ 	
+
+
+
+}
+
+
+}
--- s.java
+++ s.java
@@ -9,15 +9,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 
-public class server{
-	  public final static int FILE_SIZE = 1022386;
-
+public class s{
 public static void main(String[] args) throws Exception {
 	//this is for part 2 - maintains the number of connections
 	ArrayList<Socket> connections = new ArrayList<Socket>();
+
 	String sSocket = "";
+
 	System.out.println("Knockin up the FTP server like a mad woman's breakfast");
 	System.out.print("Oi, we need a socket to listen to m8: ");
+
 	Scanner sc = new Scanner(System.in);
 	String uSocket = sc.next();
 	System.out.println();
@@ -25,40 +26,45 @@ public static void main(String[] args) throws Exception {
 	//ServerSocket listenSocket = new ServerSocket(Integer.parseInt(uSocket));
 	System.out.println("Server listening...\nfrom the land down under \n" +
 			"on port: \n" + listenSocket + "\n");
-	
-
 	while (true){
 		Socket connectionSocket = listenSocket.accept();
 		connections.add(connectionSocket);
-		System.out.println("Ready the barbie. A m8 has connected.");
+		System.out.println("Ready the barbie a m8 has connected.");
 		System.out.println("There is " + connections.size() + " m8(s) connected.");
 		//returns a new socket object that represents server to a specific client
-		
-
 		BufferedReader inFromClient = new BufferedReader(
 			new InputStreamReader(connectionSocket.getInputStream()));
 		DataOutputStream outToClient = new DataOutputStream(
 			connectionSocket.getOutputStream());
 		String clientFile = inFromClient.readLine();
 		System.out.println("The client requested: " + clientFile);
-			
-		//for now lets assume the file is there and begin the transfer
-		File fileToSend = new File(clientFile);
-		byte [] mybytearray = new byte [(int)clientFile.length()];
-		FileInputStream fis = new FileInputStream(clientFile);
-		BufferedInputStream bis = new BufferedInputStream(fis);
-		bis.read(mybytearray,0,mybytearray.length);
-		OutputStream os = connectionSocket.getOutputStream();
-		System.out.println("M8 we are sending your file.");
-		os.write(mybytearray,0,mybytearray.length);
-		//os.flush();
-		//outToClient.flush();
-		outToClient.writeBytes(clientFile+"\n");
-		//hm
-		connectionSocket.close();
-		System.out.println("File has been sent!");
-		
 
+		//for now lets assume the file is there and begin the transfer
+		System.out.println("This is what the client entered: " + clientFile);
+		 File transferFile = new File (clientFile);
+
+		 if (transferFile.exists() == false){
+		 	outToClient.writeBytes("m8 the file doesn't exist!");
+		 	System.out.println("File does not exist");
+		 	break;
+		 }
+
+   byte [] bytearray = new byte [(int)transferFile.length()];
+    FileInputStream fin = new FileInputStream(transferFile); 
+    BufferedInputStream bin = new BufferedInputStream(fin); 
+    bin.read(bytearray,0,bytearray.length); 
+    OutputStream os = connectionSocket.getOutputStream();
+     System.out.println("Sending Files...");
+      os.write(bytearray,0,bytearray.length); 
+      os.flush(); 
+      connectionSocket.close(); 
+      System.out.println("File transfer complete"); 
+		
+		outToClient.flush();
+		System.out.println("File has been sent!");
+		//outToClient.writeBytes(clientFile+"\n");
+
+		connections.remove(connectionSocket);
 		}
 
 
